e5772877e27ace586f2d855a228a19f5ede198d8
pranayd2126 <dandapranayreddy2118@gmail.com>
2026-03-16 10:38:38 +0530

    checking git pull request

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-hello
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
