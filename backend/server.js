65536af7f6507952c58b663bf8d2eb8e8c0a41df
pranayd2126 <dandapranayreddy2118@gmail.com>
2026-03-16 12:06:45 +0530

    hello

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,60 @@
-hello     
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+connectDB();
+
+//routes
+//app.use("/users-api", userRoutes);
+
+function errorHandler(err, req, res, next) {
+  res.json({ message: "error", reason: err.message });
+}
+app.use(errorHandler);
+
+app.use((err, req, res, next) => {
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    return res.status(400).json({
+      message: "Validation failed",
+      errors: err.errors,
+    });
+  }
+  // Invalid ObjectId
+  if (err.name === "CastError") {
+    return res.status(400).json({
+      message: "Invalid ID format",
+    });
+  }
+  // Duplicate key
+  if (err.code === 11000) {
+    return res.status(409).json({
+      message: "Duplicate field value",
+    });
+  }
+  res.status(500).json({
+    message: "Internal Server Error",
+  });
+});
