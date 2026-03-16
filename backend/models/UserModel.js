c6d0a7feac265f6ee9da463df6de34d99581b806
nithin-6546 <mudumalanithin6@gmail.com>
2026-03-16 12:07:38 +0530

    Added models

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,1 +1,32 @@
-hello world
+import { Schema,model } from "mongoose";
+
+const userSchema = new Schema({
+    firstName:{
+        type:String,
+        required:[true,"First name is required"]
+    },
+    lastName:{
+        type:String
+    },
+    email:{
+        type:String,
+        required:[true,"email is required"],
+        unique:[true,"email already exists"]
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"]
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    }
+},{
+    strict:"throw",
+    timestamps:true,
+    versionKey:false
+}
+);
+
+const UserModel = model("User", userSchema);
+export default UserModel;
